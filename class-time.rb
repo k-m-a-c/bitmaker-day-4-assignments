06769acdc11baec05f43650ff3b68a2170bbb935
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-06-11 13:36:57 -0400

    define a method 'learn' for the class 'Student'

--- class-time.rb
+++ class-time.rb
@@ -2,8 +2,29 @@
 #
 # Fun n' games with Classes
 
+# Define "student"
 class Student
+
+  def initialize(name)
+    @name = name
+  end
+
+  def learn
+    "I get it!"
+
+  end
+
 end
 
+# Define "instructor"
 class Instructor
+
+  def initialize
+  end
+
 end
+
+# Print stuff
+kevin = Student.new("Kevin")
+
+puts kevin.learn
