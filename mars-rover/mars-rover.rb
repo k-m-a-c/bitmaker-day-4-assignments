c8555e24b7dfb3193463d0ceaa8bf284f7d4690f
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-06-12 23:09:12 -0400

    improve the DIRECTIONS constant and give the rover the instruction-parsing role

--- mars-rover/mars-rover.rb
+++ mars-rover/mars-rover.rb
@@ -2,16 +2,12 @@
 #
 # Exercise 2: rovin' around Mars
 
-# A rover can move, turn left and right, and show you where it is.
-
 require_relative('navigation.rb')
 
 class Rover
   include Navigation
 
   attr_reader :name, :x, :y, :heading
-  # include Navigation
-  # include Movement
 
   def initialize (name, x, y, heading)
     @name = name
@@ -21,49 +17,54 @@ class Rover
   end
 
   def move
-    change_in_x, change_in_y = DIRECTIONS[@heading]
+    # change the rover's position by adding the current values of the x and y
+    # instance variables, to the product of the 'speed' and the x and y variables
+    # local to the DIRECTIONS constant in the Navigation module
 
-    @x += change_in_x
-    @y += change_in_y
+    change_in_x, change_in_y = DIRECTIONS[@heading.to_sym]
 
+    speed = 1 # how fast the rover will move
+
+    @x += (change_in_x * speed)
+    @y += (change_in_y * speed)
   end
 
   def execute_instructions(instructions)
 
-    case instructions
-    when "M"
-      move
-    when "R"
-      turn_right
-    when "L"
-      turn_left
+    # parse a string on the individual character and interpret it as an instruction
+    instructions.each_char do |instruction|
+
+      # define what the rover should do for each individual instruction it receives
+      case instruction
+      when "M"
+        move
+      when "R"
+        turn_right
+      when "L"
+        turn_left
+      end
+      puts "<#{@name}>: [MOVED TO] #{@heading} #{@x} #{@y}"
     end
-    puts "#{@name}: [MOVED TO] #{@heading} #{@x} #{@y}"
+
   end
 
   def show_position
-    puts "#{@name}: #{@heading} #{@x} #{@y}"
+    puts "<#{@name}>: #{@heading} #{@x} #{@y}"
   end
-
-
 end
 
-rover_1 = Rover.new("Rover #1", 9, 9, "S")
-rover_2 = Rover.new("Rover #2", 0, 0, "N")
+# get the size of our plateau
+# print "What is the size of our plateau?"
+# plateau_size = gets.chomp.to_i
 
+# create a couple rovers
+rover_1 = Rover.new("rover-1", 1, 2, "N")
+rover_2 = Rover.new("rover-2", 3, 3, "E")
+
+# show where the rovers are
 rover_1.show_position
 rover_2.show_position
 
-instructions = "RML"
-
-instructions.each_char do |instruction|
-  rover_1.execute_instructions(instruction)
-end
-
-instructions.each_char do |instruction|
-  rover_2.execute_instructions(instruction)
-end
-
-
-
-
+# move the rovers
+rover_1.execute_instructions("LMLMLMLMM")
+rover_2.execute_instructions("MMRMMRMRRM")
--- mars-rover/navigation.rb
+++ mars-rover/navigation.rb
@@ -5,16 +5,16 @@ module Navigation
 
   # The definition of each heading
   DIRECTIONS = {
-      "N" => [0,1],
-      "E" => [1,0],
-      "S" => [0,-1],
-      "W" => [-1,0]
+      :N => [0,1],
+      :E => [1,0],
+      :S => [0,-1],
+      :W => [-1,0]
     }
 
   # Set a heading
   def set_heading(n=1)
-    position = COMPASS.index(@heading)
-    @heading = COMPASS.rotate(n)[position]
+    heading = COMPASS.index(@heading)
+    @heading = COMPASS.rotate(n)[heading]
   end
 
   def turn_right
